7a859a8c7d09000e5f2a4df17ca8483dadf6bf53
376624607 <31661884+376624607@users.noreply.github.com>
2018-03-06 20:48:57 +0800

    Add files via upload

--- grid_mdp.py
+++ grid_mdp.py
@@ -1,53 +1,89 @@
+import random
 class Mdp:
 
     def __init__(self):
-        #状态空间及吸收态标记
         self.states=[1,2,3,4,5,6,7,8]
         self.terminal_states=dict()
-        self.terminal_states['6'] = 1
-        self.terminal_states['7'] = 1
-        self.terminal_states['8'] = 1
-        #行动集合
-        self.actions=['e','s','w','n']
-        #计数奖励
+        self.terminal_states[6]=1
+        self.terminal_states[7] = 1
+        self.terminal_states[8] = 1
+
+        self.actions=['n','e','s','w']
+
         self.rewards=dict()
         self.rewards['1_s']=-1
         self.rewards['3_s']=1
         self.rewards['5_s']=-1
-        #转移状态
+
         self.t=dict()
-        self.t['1_s']=6
-        self.t['1_e']=2
-        self.t['2_w']=1
-        self.t['2_e']=3
-        self.t['3_w']=2
-        self.t['3_s']=7
-        self.t['3_e']=4
-        self.t['4_w']=3
-        self.t['4_e']=5
-        self.t['5_w']=4
-        self.t['5_s']=8
-        #γ衰减因子
-        self.gama=0.8
+        self.t['1_s'] = 6
+        self.t['1_e'] = 2
+        self.t['2_w'] = 1
+        self.t['2_e'] = 3
+        self.t['3_s'] = 7
+        self.t['3_w'] = 2
+        self.t['3_e'] = 4
+        self.t['4_w'] = 3
+        self.t['4_e'] = 5
+        self.t['5_s'] = 8
+        self.t['5_w'] = 4
+
+        self.gama=0.5
+
+    def getstates(self):
+        return self.states
+
+    def getactions(self):
+        return self.actions
+
+    def getterminal(self):
+        return self.terminal_states
+
+    def getgama(self):
+        return self.gama
 
     def transform(self,state,action):
         if state in self.terminal_states:
             return True,state,0
-
-        key='{}_{}'.format(str(state),action)
+        key="%d_%s"%(state,action)
         if key in self.t:
             nextstate=self.t[key]
         else:
-            nextstate=state  #碰到墙壁时因为self.t中未定义下次转换状态，故一直碰壁
+            nextstate=state
 
-        #判断是否为吸收态
         is_terminal=False
-        if nextstate in self.terminal_states:
+        if key in self.terminal_states:
             is_terminal=True
-        #判断key是否属于rewards非零的操作
-        if key in self.rewards:
-            r=self.rewards[key]
-        else:
+
+        if key not in self.rewards:
             r=0.0
+        else:
+            r=self.rewards[key]
 
         return is_terminal,nextstate,r
+
+    def gen_randompi_sample(self,num):
+        #num 样本数
+        s_sample=[]
+        a_sample=[]
+        r_sample=[]
+        for i in range(num):
+            s_temp=[]
+            a_temp=[]
+            r_temp=[]
+            s=self.states[int(random.random()*len(self.states))]
+            t=False  #用于控制是否吸收态
+            while False==t:
+                a=self.actions[int(random.random()*len(self.actions))]
+                t,s1,r=self.transform(s,a)
+                s_temp.append(s)
+                a_temp.append(a)
+                r_temp.append(r)
+                s=s1
+            s_sample.append(s_temp)
+            a_sample.append(a_temp)
+            r_sample.append(r_temp)
+
+        return s_sample,a_sample,r_sample
+
+
